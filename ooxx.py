a94fcca0224468289621e23b300854729aed6570
linyuanchi <linaustin0119@gmail.com>
2021-03-26 01:24:35 +0800

    fix arrary proble working on computer moves

--- ooxx.py
+++ ooxx.py
@@ -1,9 +1,9 @@
-board=str([[i+1+k*3 for i in range(3)] for k in range(3)])
+board=[[str(i+1+k*3) for i in range(3)] for k in range(3)]
+playerInput="A"
 
-print(board)
 
 
-def drawBoard(board):
+def drawBoard(grid):
     count = 0
     array = 0
     while count < 13:
@@ -19,11 +19,68 @@ def drawBoard(board):
 
         else:
             for i in range(3):
-                print("|   ",board[array][i],"   ",sep="",end="")
+                print("|   ",grid[array][i],"   ",sep="",end="")
             print("|")
             array+=1
 
         count+=1
 
-drawBoard(board)
+
+def playerMove(grid):
+    inputChecker=False
+
+    while not inputChecker:
+        choose=str(input("Make a move :"))
+
+        for i in range(3):
+            for k in range(3):
+                if(grid[i][k]==choose):
+                    grid[i][k]="O"
+                    inputChecker=True
+    return grid
+
+def computerMove(grid):
+    blockValue=[[0 for i in range(3)] for k in range(3)]
+    
+    for i in range(3):
+        for k in range(3):
+            if(grid[i][k]!="O"):
+                if(k==0 and grid[i][k+1]==grid[i][k+2]):
+                    if(grid[i][k+2]=="X"):
+                        blockValue[i][k]+=1000
+                    else:
+                        blockValue[i][k]+=900
+
+                if(k==1 and grid[i][k-1]==grid[i][k+1]):
+                    if(grid[i][k+1]=="X"):
+                        blockValue[i][k]+=1000
+                    else:
+                        blockValue[i][k]+=900
+
+                if(k==2 and grid[i][k-1]==grid[i][k-2]):
+                    if(grid[i][k-1]=="X"):
+                        blockValue[i][k]+=1000
+                    else:
+                        blockValue[i][k]+=900
+
+                if(i==0 and grid[i+1][k]==grid[i+2][k]):
+                    if(grid[i+1][k]=="X"):
+                        blockValue[i][k]+=1000
+                    else:
+                        blockValue[i][k]+=900
+
+                if(i==1 and grid[i-1][k]==grid[i+1][k]):
+                    if(grid[i+1][k]=="X"):
+                        blockValue[i][k]+=1000
+                    else:
+                        blockValue[i][k]+=900
+
+                if(i==2 and grid[i-1][k]==grid[i-2][k]):
+                    if(grid[i-1][k]=="X"):
+                        blockValue[i][k]+=1000
+                    else:
+                        blockValue[i][k]+=900
+
+    
+
 
